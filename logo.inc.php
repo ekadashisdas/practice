1500f188ded48d61fd38857bc56d982ff96b6776
ekadashisdas <111307814+ekadashisdas@users.noreply.github.com>
2022-08-16 21:16:05 +0500

    practice

---
+++ logo.inc.php
@@ -0,0 +1,5 @@
+
+                <div class="logo"> 
+                <img src="//sysrtfm.ru/wp-content/uploads/2012/01/php-logo1.png" alt="php">
+                </div>                                                 
+        
